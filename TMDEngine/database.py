cc5538a843287d699a01440db3032796a2171d8d
Kanwar Jot Parkash Singh <52690070+kanwarjotp@users.noreply.github.com>
2023-04-20 18:37:25 +0530

    CREATED change_song() function
    changing names in song tables just got easier.

--- TMDEngine/database.py
+++ TMDEngine/database.py
@@ -64,7 +64,8 @@ def insert_song(song_name):
     """
 
     :param song_name: the name of the song, to be added to the database
-    :return: song_id and fingerprinted_val: returns the song_id and fingerprinted value from the database, after updating the database
+    :return: song_id and fingerprinted_bool: returns the song_id and fingerprinted boolean(0: not fingerprinted,
+    1: fingerprinted) from the database, after updating the database
 
     """
     db_conn = sqlite3.connect(DATABASE_NAME)
@@ -77,9 +78,8 @@ def insert_song(song_name):
         db_conn.commit()
     except sqlite3.DatabaseError as e:
         if "UNIQUE constraint failed:" in e.__str__():
-            print("Song already in Database\n(SongID\tFingerprinted{0:N/1:Y})")
+            print("Song already in Database with \n(SongID\tFingerprinted{0:N/1:Y})")
         else:
-            print(sqlite_cmd)
             raise e
 
     # extracting the song_id
@@ -88,6 +88,7 @@ def insert_song(song_name):
     db_cursor.execute(sqlite_cmd)
 
     rows = db_cursor.fetchall()
+    db_conn.close()
 
     s_id = None
     f_val = None
@@ -98,7 +99,7 @@ def insert_song(song_name):
     return s_id, f_val
 
 
-def display_fingerprint_table(hash_lookup):
+def find_hash(hash_lookup):
     db_conn = sqlite3.connect(DATABASE_NAME)
     db_conn.row_factory = sqlite3.Row
 
@@ -113,5 +114,21 @@ def display_fingerprint_table(hash_lookup):
         print(entry['hash'], entry['song_id'], entry['offset'])
 
 
-print(insert_song("katy_perry"))
+def change_song_name(song_name, new_song_name):
+    db_conn = sqlite3.connect(DATABASE_NAME)
+    db_cursor = db_conn.cursor()
 
+    sqlite_cmd = '''UPDATE song SET song_name = "{0}" WHERE song_name = "{1}"'''.format(new_song_name, song_name)
+    db_cursor.execute(sqlite_cmd)
+    db_conn.commit()
+
+    # checking change
+    db_conn.row_factory = sqlite3.Row
+    sqlite_cmd = '''SELECT * FROM song WHERE song_name = "{0}"'''.format(new_song_name)
+    db_cursor.execute(sqlite_cmd)
+    rows = db_cursor.fetchall()
+
+    if rows:
+        print("Successful")
+    else:
+        print("No entry with song_name = {0}".format(song_name))
