e6b32fa983d39af48c316df4792c1178be50796f
Kanwar Jot Parkash Singh <52690070+kanwarjotp@users.noreply.github.com>
2023-04-28 05:03:37 +0530

    hash creation for a recorded sample, hash lookup in database, time difference evaluation, all have been implemented.

--- TMDEngine/run.py
+++ TMDEngine/run.py
@@ -1,1 +1,29 @@
 # final run script for using the recognition application
+import fingerprint
+import recognize
+import dev.config as conf
+
+user_sample = conf.test_rec   # path to user's recording
+
+fprinter_for_sample = fingerprint.Fingerprint(user_sample)
+fprints_of_sample = fprinter_for_sample.get_fingerprint(verbose=True)  # the fingerprints for sample
+print("Fingerprints Created")
+
+# lookup the fingerprints using hash matching
+matching_fingerprints_in_db = recognize.look_for_matches(fprints_of_sample[:2])  # trying at first on a2 fingerprints
+print("Matching Fingerprints Found")
+
+
+# align the fingerprints received from the database and store them as (song_id, time_difference)
+aligned_matching_fingerprints = []
+for fingerprint_of_sample in matching_fingerprints_in_db.keys():
+    aligned_matching_fingerprints.append(
+        recognize.align_matches(
+            fingerprint_of_sample, matching_fingerprints_in_db[fingerprint_of_sample]
+        )
+    )
+print("Fingerprints aligned")
+
+# once the matches have been aligned, just find the highest count for a difference, that one is the song_id
+count_of_vals_for_diff = {}
+print(aligned_matching_fingerprints)
--- TODO.md
+++ TODO.md
@@ -6,14 +6,16 @@ TMDEngine TODO list.
 
 - [ ] Find a server to host your website.
 - [ ] Create a workable recorder, at present the recordings from record.py are useless.
+- [ ] Decrease the loookup time for matching fingerprints, uit takes 8 min+ for a sample with 678 fingerprints 
 
 ### In Progress
 
-- [ ] Create the aligning function for fingerprints.
 
 ### Done ✓
 
 - [x] Update sql_database to integrate it with the rest of the application.
 - [x] Create the build script for the app.
 - [x] Adding fingerprints to the database
+- [x] Create the aligning function for fingerprints.
+
 
