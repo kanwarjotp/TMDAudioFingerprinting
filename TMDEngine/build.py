affc831ec724922b9a00327d67f9ae22326e7cb8
Kanwar Jot Parkash Singh <52690070+kanwarjotp@users.noreply.github.com>
2023-04-27 06:36:30 +0530

    created build.py
    open song files, add song_names to database, create fingerprints, add them to database.

--- TMDEngine/build.py
+++ TMDEngine/build.py
@@ -1,1 +1,54 @@
+import os
+from TMDEngine.dev import config
+import sql_database as db
+import fingerprint
+
+
 # create fingerprints of known songs and add to database
+
+# get files and names of songs
+def get_files(address_to_files_folder: str):
+    """
+    A function that accesses the local folder containing the .wav files and save their path and names
+    :param address_to_files_folder: local directory containing wav files
+    :return: a dictionary with key values as song_address names and values as path to the files
+    """
+    songs = {}
+    with os.scandir(address_to_files_folder) as folder:
+        for file in folder:
+            songs[file.name[:-4]] = file.path.replace("\\", "/")
+    return songs
+
+
+def add_song_to_app(name: str, song_address: str):
+    """
+    A function to update the song_address info and related fingerprints(after their creation) to the Engine Database.
+    Any DUPLICATE songs will not be added to database
+    :param name: name of the song, this will be stored in the song table of the database
+    :param song_address: the local path to the .wav file fo the song_address
+    :return:
+    """
+    database_cnxn = db.SQLConnection()
+    song_id = database_cnxn.insert_song(name)
+    num_duplicates = 0
+    if song_id != -1:  # not a DUPLICATE
+
+        f = fingerprint.Fingerprint(song_address, song_id)
+        fingerprints = f.get_fingerprint(plot=False, verbose=True)
+
+        for fprint in fingerprints:
+            num_duplicates += database_cnxn.insert_fingerprint(fprint)
+
+        database_cnxn.change_fingerprinted_flag(song_id, True)
+        print("Unique Fingerprints for {0}, id {1} inserted: {2}.\n Originally {3} duplicates \n\n".
+              format(name, song_id, len(fingerprints) - num_duplicates, num_duplicates))
+    else:
+        print("Song already present in database")
+
+
+song_info_dict = get_files(config.files_folder)
+
+for song_name in song_info_dict.keys():
+    print(song_name, song_info_dict[song_name])
+
+    add_song_to_app(song_name, song_info_dict[song_name])
